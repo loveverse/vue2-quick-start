7d0ba1eb1233c45c7a81154a1100b88e82323492
loveverse <398096830@qq.com>
2023-04-06 18:24:03 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require("@vue/cli-service");
+// const path = require("path");
 module.exports = defineConfig({
   transpileDependencies: true,
+  productionSourceMap: false,
+  // configureWebpack: {
+  //   resolve: {
+  //     extensions: [".js", ".vue", ".json"],
+  //     alias: {
+  //       "@": path.resolve(__dirname, "./src"),
+  //     },
+  //   },
+  // },
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "@/assets/css/base.scss";`,
+      },
+    },
+  },
 });
